d1dc94a66967d8899fdfac92d65ef1965dea9e9f
Mingalimov Damir <damdamka@gmail.com>
2018-04-28 13:44:04 +0300

    Successful load of file

--- scp_h-petrovich.py
+++ scp_h-petrovich.py
@@ -3,10 +3,13 @@ from scp import SCPClient
 
 h_petr = SSHClient()
 h_petr.load_system_host_keys()
-h_petr.connect('85.114.0.138')
+h_petr.connect('85.114.0.138',
+				password='',
+				username='')
 
 print(h_petr.get_transport())
 
 with SCPClient(h_petr.get_transport()) as squid:
 
-	squid.get('/usr/local/etc/squid/squid.conf', local_path='files/')
+	squid.get('/usr/local/etc/squid/squid.conf', 
+				local_path='/home/damir/Python/files/squid.conf')
